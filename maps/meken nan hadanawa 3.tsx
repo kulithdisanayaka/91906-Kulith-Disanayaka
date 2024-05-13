1f1da0ad5b4976391b78c93ac7d3acc0dc7f92a3
kulithdisanayaka <kd23673@my.westlake.school.nz>
2024-05-13 16:40:02 +1200

    Fixed tilemap error 2

--- maps/meken nan hadanawa 3.tsx
+++ maps/meken nan hadanawa 3.tsx
@@ -2,114 +2,114 @@
 <tileset version="1.10" tiledversion="1.10.2" name="meken nan hadanawa 3" tilewidth="175" tileheight="190" tilecount="37" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="7" height="5" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/6.png"/>
+  <image width="7" height="5" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/6.png"/>
  </tile>
  <tile id="1">
-  <image width="7" height="7" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/7.png"/>
+  <image width="7" height="7" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/7.png"/>
  </tile>
  <tile id="2">
-  <image width="7" height="5" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/8.png"/>
+  <image width="7" height="5" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/8.png"/>
  </tile>
  <tile id="3">
-  <image width="6" height="5" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/9.png"/>
+  <image width="6" height="5" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/9.png"/>
  </tile>
  <tile id="4">
-  <image width="8" height="6" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/1.png"/>
+  <image width="8" height="6" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/1.png"/>
  </tile>
  <tile id="5">
-  <image width="8" height="6" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/2.png"/>
+  <image width="8" height="6" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/2.png"/>
  </tile>
  <tile id="6">
-  <image width="8" height="7" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/3.png"/>
+  <image width="8" height="7" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/3.png"/>
  </tile>
  <tile id="7">
-  <image width="6" height="5" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/4.png"/>
+  <image width="6" height="5" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/4.png"/>
  </tile>
  <tile id="8">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/5.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/5.png"/>
  </tile>
  <tile id="9">
-  <image width="10" height="7" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/10.png"/>
+  <image width="10" height="7" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/10.png"/>
  </tile>
  <tile id="10">
-  <image width="7" height="5" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/11.png"/>
+  <image width="7" height="5" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/11.png"/>
  </tile>
  <tile id="11">
-  <image width="7" height="5" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/12.png"/>
+  <image width="7" height="5" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/12.png"/>
  </tile>
  <tile id="12">
-  <image width="5" height="4" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/13.png"/>
+  <image width="5" height="4" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/13.png"/>
  </tile>
  <tile id="13">
-  <image width="8" height="6" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/14.png"/>
+  <image width="8" height="6" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/14.png"/>
  </tile>
  <tile id="14">
-  <image width="7" height="6" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/15.png"/>
+  <image width="7" height="6" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Grass/15.png"/>
  </tile>
  <tile id="15">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/1.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/1.png"/>
  </tile>
  <tile id="16">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/2.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/2.png"/>
  </tile>
  <tile id="17">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/3.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/3.png"/>
  </tile>
  <tile id="18">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/4.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/4.png"/>
  </tile>
  <tile id="19">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/5.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/5.png"/>
  </tile>
  <tile id="20">
-  <image width="8" height="8" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/6.png"/>
+  <image width="8" height="8" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Leaf/6.png"/>
  </tile>
  <tile id="21">
-  <image width="32" height="25" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Box.png"/>
+  <image width="32" height="25" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Box.png"/>
  </tile>
  <tile id="22">
-  <image width="15" height="13" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Garbage_Can1.png"/>
+  <image width="15" height="13" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Garbage_Can1.png"/>
  </tile>
  <tile id="23">
-  <image width="15" height="13" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Garbage_Can2.png"/>
+  <image width="15" height="13" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Garbage_Can2.png"/>
  </tile>
  <tile id="24">
-  <image width="23" height="32" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ladder1.png"/>
+  <image width="23" height="32" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ladder1.png"/>
  </tile>
  <tile id="25">
-  <image width="23" height="32" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ladder2.png"/>
+  <image width="23" height="32" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ladder2.png"/>
  </tile>
  <tile id="26">
-  <image width="92" height="48" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ramp1.png"/>
+  <image width="92" height="48" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ramp1.png"/>
  </tile>
  <tile id="27">
-  <image width="92" height="48" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ramp2.png"/>
+  <image width="92" height="48" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Ramp2.png"/>
  </tile>
  <tile id="28">
-  <image width="144" height="39" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Rapm3.png"/>
+  <image width="144" height="39" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Rapm3.png"/>
  </tile>
  <tile id="29">
-  <image width="26" height="7" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard1.png"/>
+  <image width="26" height="7" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard1.png"/>
  </tile>
  <tile id="30">
-  <image width="11" height="20" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard2.png"/>
+  <image width="11" height="20" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard2.png"/>
  </tile>
  <tile id="31">
-  <image width="10" height="20" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard3.png"/>
+  <image width="10" height="20" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard3.png"/>
  </tile>
  <tile id="32">
-  <image width="12" height="20" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard4.png"/>
+  <image width="12" height="20" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Skateboard4.png"/>
  </tile>
  <tile id="33">
-  <image width="62" height="103" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree1.png"/>
+  <image width="62" height="103" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree1.png"/>
  </tile>
  <tile id="34">
-  <image width="124" height="129" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree2.png"/>
+  <image width="124" height="129" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree2.png"/>
  </tile>
  <tile id="35">
-  <image width="175" height="190" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree3.png"/>
+  <image width="175" height="190" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree3.png"/>
  </tile>
  <tile id="36">
-  <image width="175" height="190" source="C:/Users/disak/Downloads/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree4.png"/>
+  <image width="175" height="190" source="tile set/craftpix-net-362692-free-green-zone-tileset-pixel-art/3 Objects/Other/Tree4.png"/>
  </tile>
 </tileset>
